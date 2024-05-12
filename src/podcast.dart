a884de80942e965a8d17926a22c0cda276d2ce23
Aaron Stivens Hernandez <aaronhernandez14963@gmail.com>
2024-05-12 15:25:55 -0600

    Pagina de podcast terminada

--- src/podcast.dart
+++ src/podcast.dart
@@ -7,9 +7,59 @@ class Podcast extends StatelessWidget {
       appBar: AppBar(
         title: Text('Podcast'),
       ),
-      body: Center(
-        child: Text('Página de podcast'),
-      ),
+      body: ReproductorPodcast(),
+    );
+  }
+}
+
+class ReproductorPodcast extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CircleAvatar(
+          radius: 80,
+          backgroundColor: Colors.grey[300],
+          child: Icon(
+            Icons.headset,
+            size: 100,
+            color: Colors.grey[600],
+          ),
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Nombre del Podcast',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Descripción del episodio o podcast',
+          style: TextStyle(fontSize: 16),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.skip_previous),
+              iconSize: 40,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.play_arrow),
+              iconSize: 60,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.skip_next),
+              iconSize: 40,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
